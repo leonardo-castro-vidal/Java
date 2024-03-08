6c6ea4601e156535c55e5cd8e2aab1ffa1dea8d6
Leonardo Castro <leonardo.castro.vidall@gmail.com>
2024-03-08 15:36:27 -0300

    Primer Commit.

---
+++ src/hackerrank/ejemplo/Main.java
@@ -0,0 +1,16 @@
+package hackerrank.ejemplo;
+/*
+*@Usuario  : 56993
+*@Fecha    : 08-03-2024
+*@Archivo  : Main.java
+*@Paquete  : hackerrank.ejemplo
+*@Proyecto : Java
+*/
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
