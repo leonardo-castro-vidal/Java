08ee6d9533509406908e86f678341381798abe05
Leonardo Castro <leonardo.castro.vidall@gmail.com>
2024-03-08 15:45:27 -0300

    Segundo Commit.

---
+++ src/hackerrank/ejemplo/HelloWorld.java
@@ -0,0 +1,11 @@
+package hackerrank.ejemplo;
+/*
+*@Usuario  : 56993
+*@Fecha    : 08-03-2024
+*@Archivo  : HelloWorld.java
+*@Paquete  : hackerrank.ejemplo
+*@Proyecto : Java
+*/
+public class HelloWorld {
+
+}
